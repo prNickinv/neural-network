a503a236098a527ff27fb8d963c85acaec961793
Nikita <nikitartamonov2112@gmail.com>
2024-04-22 18:26:15 +0300

    Add training parameters

--- MnistTest.cpp
+++ MnistTest.cpp
@@ -10,18 +10,26 @@ namespace NeuralNetwork::MnistTest {
 
 void RunMnistTest(Data::MnistType mnist_type,
                   Data::DataProcessing data_processing) {
+  int train_size = 60000;
+  int val_size = 0;
+  int test_size = 10000;
   auto [train_inputs, train_targets, val_inputs, val_targets, test_inputs,
         test_targets] =
-      NeuralNetwork::Data::GetMnistData(mnist_type, 60000, 0, 10000,
-                                        data_processing);
+      NeuralNetwork::Data::GetMnistData(mnist_type, train_size, val_size,
+                                        test_size, data_processing);
 
+  int batch_size = 4;
+  double learning_rate = 0.07;
+  double weights_decay = 0.001;
+  int epochs = 5;
   auto network =
       NeuralNetwork::Network({784, 128, 10},
                              {NeuralNetwork::ActivationFunction::LeakyReLu(),
                               NeuralNetwork::ActivationFunction::SoftMax()});
 
-  network.Train(train_inputs, train_targets, test_inputs, test_targets, 4, 0.07,
-                0.001, 5, NeuralNetwork::LossFunction::CrossEntropyLoss(),
+  network.Train(train_inputs, train_targets, test_inputs, test_targets,
+                batch_size, learning_rate, weights_decay, epochs,
+                NeuralNetwork::LossFunction::CrossEntropyLoss(),
                 NeuralNetwork::Task::SoftMaxCEClassification);
 }
 
