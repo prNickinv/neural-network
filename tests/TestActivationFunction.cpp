6a12ef10057da003688e89a7831bf3bb403e2faa
Nikita <nikitartamonov2112@gmail.com>
2024-05-04 14:15:27 +0300

    Fix codestyle in tests

--- tests/TestActivationFunction.cpp
+++ tests/TestActivationFunction.cpp
@@ -6,29 +6,28 @@
 using Vector = Eigen::VectorXd;
 
 TEST(Relu, PosInputActivation) {
-    auto relu = NeuralNetwork::ActivationFunction::ReLu();
-    Vector input(5);
-    input << 1, 3, 154, 12, 100;
-    Vector expected_output(5);
-    expected_output << 1, 3, 154, 12, 100;
-    EXPECT_EQ(expected_output, relu.Activate(input));
+  auto relu = NeuralNetwork::ActivationFunction::ReLu();
+  Vector input(5);
+  input << 1, 3, 154, 12, 100;
+  Vector expected_output(5);
+  expected_output << 1, 3, 154, 12, 100;
+  EXPECT_EQ(expected_output, relu.Activate(input));
 }
 
 TEST(Relu, ZeroInputActivation) {
-    auto relu = NeuralNetwork::ActivationFunction::ReLu();
-    Vector input(5);
-    input << 0, 0, 0, 0, 0;
-    Vector expected_output(5);
-    expected_output << 0, 0, 0, 0, 0;
-    EXPECT_EQ(expected_output, relu.Activate(input));
-
+  auto relu = NeuralNetwork::ActivationFunction::ReLu();
+  Vector input(5);
+  input << 0, 0, 0, 0, 0;
+  Vector expected_output(5);
+  expected_output << 0, 0, 0, 0, 0;
+  EXPECT_EQ(expected_output, relu.Activate(input));
 }
 
 TEST(ReLu, NegInputActivation) {
-    auto relu = NeuralNetwork::ActivationFunction::ReLu();
-    Vector input(5);
-    input << -1, -3, -154, -12, -100;
-    Vector expected_output(5);
-    expected_output << 0, 0, 0, 0, 0;
-    EXPECT_EQ(expected_output, relu.Activate(input));
+  auto relu = NeuralNetwork::ActivationFunction::ReLu();
+  Vector input(5);
+  input << -1, -3, -154, -12, -100;
+  Vector expected_output(5);
+  expected_output << 0, 0, 0, 0, 0;
+  EXPECT_EQ(expected_output, relu.Activate(input));
 }
