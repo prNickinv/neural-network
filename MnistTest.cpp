3b1e85ae5f2a1d40559fd65a2dea57dc5d450b26
Nikita <nikitartamonov2112@gmail.com>
2024-05-26 13:59:32 +0300

    Refactor Mnisttest.cpp

--- MnistTest.cpp
+++ MnistTest.cpp
@@ -17,11 +17,10 @@ void RunMnistTest(Data::MnistType mnist_type,
   int val_size = 0;
   int test_size = 10000;
   auto [train_inputs, train_targets, val_inputs, val_targets, test_inputs,
-        test_targets] =
-      NeuralNetwork::Data::GetMnistData(mnist_type, train_size, val_size,
-                                        test_size, data_processing);
-//    std::ifstream file("/Users/nikitaartamonov/CLionProjects/network_test_momentum.txt");
-//    NeuralNetwork::Network network(file);
+        test_targets] = Data::GetMnistData(mnist_type, train_size, val_size,
+                                           test_size, data_processing);
+  //    std::ifstream file("/Users/nikitaartamonov/CLionProjects/network_test_momentum.txt");
+  //    NeuralNetwork::Network network(file);
 
   int batch_size = 4;
   double learning_rate = 0.01;
@@ -32,17 +31,16 @@ void RunMnistTest(Data::MnistType mnist_type,
   int epochs = 10;
 
   auto network =
-      NeuralNetwork::Network({784, 25, 100, 10},
-                             {NeuralNetwork::ActivationFunction::LeakyReLu(),
-                              NeuralNetwork::ActivationFunction::LeakyReLu(),
-                              NeuralNetwork::ActivationFunction::SoftMax()});
-  network.SetOptimizer(NeuralNetwork::AdamWOptimizer(learning_rate, weights_decay, beta1, beta2, epsilon));
+      Network({784, 25, 100, 10},
+              {ActivationFunction::LeakyReLu(), ActivationFunction::LeakyReLu(),
+               ActivationFunction::SoftMax()});
+  network.SetOptimizer(
+      AdamWOptimizer(learning_rate, weights_decay, beta1, beta2, epsilon));
   network.Train(train_inputs, train_targets, test_inputs, test_targets,
-                batch_size, epochs,
-                NeuralNetwork::LossFunction::CrossEntropyLoss(),
-                NeuralNetwork::Task::SoftMaxCEClassification);
-//    std::ofstream fileout("/Users/nikitaartamonov/CLionProjects/network_test_momentum.txt");
-//    fileout << network;
+                batch_size, epochs, LossFunction::CrossEntropyLoss(),
+                Task::SoftMaxCEClassification);
+  //    std::ofstream fileout("/Users/nikitaartamonov/CLionProjects/network_test_momentum.txt");
+  //    fileout << network;
 }
 
 void RunClassicMnistTest() {
